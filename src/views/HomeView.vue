4a1e6ec1e1fa55fde49f31d724c1fea93b843237
Syaifuddin091 <syaifuddin091@gmail.com>
2024-05-13 20:02:50 +0700

    ibnu

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,175 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
+    <div class="navbar">
+      <nav class="nav">
+        <router-link to="/" class="active">TodoList</router-link>
+        <router-link to="/PostView">Posts</router-link>
+      </nav>
+    </div>
+  <main class="app">
+    <section class="greeting">
+      <h3 class="title">✍️Aplikasi ToDo List </h3>
+    </section>
+
+    <div class="input-section">
+      <section class="create-todo">
+        <form @submit.prevent="addTodo">
+          <h3 class="do-it">Apa yang ingin kamu lakukan🙂?</h3>
+          <input
+            type="text"
+            placeholder="Tambahkan kegiatan"
+            v-model="text"
+          />
+
+          <input type="submit" value="Add" />
+        </form>
+      </section>
+    </div>
+
+    <h2 class="centered-text" v-show="todos.length === 0">Tidak ada kegiatan😞</h2>
+
+    <div class="todo-section">
+      <section class="todo-list">
+        <button class="hide" @click="hideCompleted = !hideCompleted">{{ hideCompleted ? 'Show all' : 'Hide completed' }}</button>  
+
+          
+        <div class="edit-todo" v-if="editingTodo">
+          <form @submit.prevent="saveEdit">
+            <input
+            type="text"
+            v-model="editingTodo.todo"
+            />
+            <button class="save-button" type="submit">Simpan</button>
+            <button class="cancel-button" @click="cancelEdit">Batal</button>
+          </form>
+        </div>
+        
+
+        <div class="list">
+          <div
+            v-for="todo in filter" :class="`todo-item ${todo.done && 'done'}`">
+            <label v-show="!editingTodo">
+              <input class="edit"type="checkbox" v-model="todo.done" />
+            </label>
+
+            <div class="todo-content">
+              <ul>
+                <li>{{ todo.todo }}</li>
+              </ul>
+            </div>
+
+
+            <div class="actions">
+              <button class="edit" @click="editTodo(todo)">Edit</button>
+              <button class="delete" @click="deleteTodo(todo)">Hapus</button>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
   </main>
 </template>
+
+
+<script setup>
+import { ref, onMounted, watch, computed } from "vue";
+
+const todos = ref([]);
+const text = ref("");
+const hideCompleted = ref(false);
+const editingTodo = ref(null);
+
+const filter = computed(() => {
+  return hideCompleted.value ? todos.value.filter(todo => !todo.done) : todos.value;
+});
+
+
+function addTodo() {
+  if (text.value.trim() === "") {
+    return;
+  }
+
+  todos.value.unshift({
+    todo: text.value,
+    done: false,
+  });
+
+  text.value = "";
+  showAddForm.value = false;
+}
+
+function deleteTodo(todo) {
+  todos.value = todos.value.filter((x) => x !== todo);
+}
+
+function editTodo(todo) {
+  if (todo.done) {
+    alert("Kegiatan yang sudah selesai tidak dapat diedit!");
+    return;
+  }
+
+  editingTodo.value = todo;
+  document.querySelectorAll('.edit').forEach(element => {
+    element.style.display = 'none';
+  });
+}
+
+function saveEdit() {
+  editingTodo.value = null;
+  document.querySelectorAll('.edit').forEach(element => {
+    element.style.display = 'block';
+  });
+}
+
+function cancelEdit() {
+  editingTodo.value = null;
+
+  document.querySelectorAll('.edit').forEach(element => {
+    element.style.display = 'block';
+  });
+}
+
+watch(
+  todos,
+  (newTodoValue) => {
+    localStorage.setItem("todos", JSON.stringify(newTodoValue));
+  },
+  { deep: true }
+);
+
+onMounted(() => {
+  todos.value = JSON.parse(localStorage.getItem("todos")) || [];
+});
+
+
+</script> 
+
+
+<style scoped>
+
+.navbar {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  padding: 2cap 0%;
+  z-index: 1000; 
+  background-color: rgb(17, 16, 101);
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+
+.navbar a {
+  font-size: 1.4rem;
+  color: rgb(255, 255, 255);
+  margin: 0 2cap; 
+  font-weight: 700;
+  text-decoration: none; 
+}
+
+.navbar .active{
+  color: #00aaff;
+}
+
+</style>
---
+++ src/views/PostView.vue
@@ -0,0 +1,207 @@
+<template>
+  <div class="container">
+    <header class="header">
+      <nav class="navbar">
+        <router-link to="/" >TodoList</router-link>
+        <router-link to="/PostView" class="active">Posts</router-link>
+      </nav>
+    </header>
+    <h3 class="post-1">✍️Post</h3>
+    <div class="posts" id="post">
+      <h1>Postingan dari :</h1>
+      <h2>{{ selectedUserName }}</h2> 
+      <select v-model="selectedUser" class="selectModel" @change="fetchPosts">
+        <option v-for="user in users" :value="user.id" :key="user.id">{{ user.name }}</option>
+      </select>
+
+      <div v-if="isLoading">
+        <div class="loading-message">Loading...</div>
+      </div>
+
+      <ul v-if="!isLoading"> 
+        <li v-for="post in posts" :key="post.id">
+          <h3>{{ post.title }}</h3>
+          <p>{{ post.body }}</p>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted, watch } from 'vue';
+
+const isLoading = ref(false); 
+const selectedUser = ref(null);
+const selectedUserName = ref('');
+const users = ref([]);
+const posts = ref([]);
+
+const fetchUser = async () => {
+  try {
+    isLoading.value = true; 
+    const response = await fetch('https://jsonplaceholder.typicode.com/users');
+    const data = await response.json();
+    users.value = data;
+  } catch (error) {
+    console.error('Error fetching users:', error);
+  } finally {
+    isLoading.value = false; 
+  }
+};
+
+const fetchPosts = async () => {
+  if (!selectedUser.value) return;
+  isLoading.value = true;
+  try {
+    const response = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${selectedUser.value}`);
+    const data = await response.json();
+    posts.value = data;
+  } catch (error) {
+    console.error('Error fetching posts:', error);
+  } finally {
+    isLoading.value = false; 
+  }
+};
+
+onMounted(() => {
+  fetchUser();
+});
+
+watch(selectedUser, () => {
+  posts.value = []; 
+  fetchPosts();
+  const selectedUserObject = users.value.find(user => user.id === selectedUser.value);
+  selectedUserName.value = selectedUserObject ? selectedUserObject.name : '';
+}, { immediate: true }); 
+</script>
+
+<style scoped>
+/* Global styles */
+@media (prefers-color-scheme: dark) {
+  .todo-app h2 ul li {
+    color: #000000; 
+  }
+}
+
+/* Section styles */
+section {
+  min-height: 100vh;
+  padding: 10rem 9% 2rem; 
+}
+.navbar .active{
+  color: #00aaff;
+}
+
+.navbar a {
+  font-size: 1.4rem;
+  color: rgb(255, 255, 255);
+  margin: 0 2cap; 
+  font-weight: 700;
+  text-decoration: none; 
+}
+
+
+.header {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  padding: 2cap 0%;
+  z-index: 1000; 
+  background-color: rgb(17, 16, 101);
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 20px;
+}
+
+.container {
+  width: 100%;
+  min-height: 100vh;
+  padding: 10px;
+  margin-top: -10px;
+  margin-bottom: -10px;
+}
+
+.posts {
+  width: 100%;
+  max-width: 900px;
+  background-color: transparent;
+  margin: 50px auto 20px;
+  padding: 40px 40px 70px;
+  border-radius: 5px;
+  border: 1px solid rgb(47, 47, 47);
+}
+
+.posts h1{
+  color: rgb(255, 255, 255);
+  margin-bottom: 20px;
+}
+
+.posts h2{
+  color: rgb(255, 255, 255);
+  margin-bottom: 20px;
+}
+
+
+.loading-message {
+  display: flex;
+  justify-content:center;
+  text-align: center;
+  margin-bottom: 10px;
+  max-width: 100px;
+  background-color: rgb(17, 16, 101);
+  border-radius: 10px;
+  padding: 2px;
+}
+
+.selectModel {
+  margin-bottom: 20px;
+  width: 100%;
+  padding: 10px;
+  font-size: 20px;
+  text-align:left;
+  border: 1px solid #545454;
+  border-radius: 5px;
+  background-color: #fff;
+  cursor: pointer;
+}
+
+.selectModel:focus {
+  outline: none;
+  border-color: #00aaff; 
+}
+
+.posts ul {
+  list-style-type: none; 
+  padding-left: 0; 
+  background-color: rgb(17, 16, 101);
+  padding: 10px;
+  border-radius: 10px;
+}
+
+.posts li {
+  color: rgb(0, 0, 0);
+  margin-bottom: 10px; 
+}
+
+.posts h3, .posts p {
+  text-align: left; 
+}
+
+.posts p {
+  padding: 0 0 0 25px;
+  color: rgb(255, 255, 255);
+  font-family: Arial, Helvetica, sans-serif;
+  font-style: italic;
+}
+
+.post-1 {
+  align-items: center;
+  text-align: center;
+  color: white;
+  font-size: 1.5rem;
+  font-weight: 700;
+  margin-top: 5.5rem;
+}
+</style>
